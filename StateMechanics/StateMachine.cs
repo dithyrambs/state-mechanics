bff9d1f6bd12945bbeb6ddc3caafd43ab6d5c84a
Alex Belzer <37421414+dithyrambs@users.noreply.github.com>
2024-09-24 17:04:02 -0700

    Code cleanup pass

--- StateMechanics/StateMachine.cs
+++ StateMechanics/StateMachine.cs
@@ -1,55 +1,106 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace StateMechanics
 {
+    /// <summary>
+    /// A scalable state-machine that requires minimal boiler-plate. States are accessed by type,
+    /// similar to how components are accessed in Unity. Users can either populate states from 
+    /// inner classes, or add them manually by type.
+    /// </summary>
     public sealed class StateMachine<T> where T : class
     {
-        public State<T> CurrentState => currentState;
-        
-        private State<T> currentState;
-        
-        private Action<State<T>> stateSwitch;
+        /// <summary>
+        /// Event invoked when the state is switched to a new state. eg. can print to console for debugging.
+        /// </summary>
+        private event Action<State<T>> StateSwitched;
 
-        private T stateMechanic = null;
-        private Dictionary<Type, State<T>> stateLookUp = new Dictionary<Type, State<T>>();
+        /// <summary>
+        /// Event invoked if the state to be switched to cannot be found. eg. can use to throw errors.
+        /// </summary>
+        private event Action<Type> InvalidStateSwitchAttempt;
 
+        private readonly Dictionary<Type, State<T>> _stateLookUp = new();
+
+        private readonly T _stateMechanic = null;
+
+        private State<T> _currentState;
+
+        /// <summary>
+        /// Get the currently running state of the state machine.
+        /// </summary>
+        public State<T> CurrentState => this._currentState;
+
+        /// <summary>
+        /// Get the type of the currently running state of the state machine.
+        /// </summary>
+        public Type currentStateType => this._currentState.GetType();
+
+        /// <summary>
+        /// On construction, pass in the client operating this machine.
+        /// </summary>
         public StateMachine(T stateMechanic)
         {
-            this.stateMechanic = stateMechanic;
-        }
-
-        public void SetMechanic(T stateMechanic)
-        {
-            this.stateMechanic = stateMechanic;
-        }
-
-        public void CleanUp()
-        {
-            currentState.CleanUp();
-        }
-
-        public void Update()
-        {
-            currentState.Update();
-        }
-
-        public void RegisterSwitchStateCallback(Action<State<T>> callback)
-        {
-            stateSwitch += callback;
-        }
-
-        public void UnregisterSwitchStateCallback(Action<State<T>> callback)
-        {
-            stateSwitch -= callback;
+            this._stateMechanic = stateMechanic;
         }
 
         /// <summary>
-        /// Automatically create states from inner classes
+        /// Call when the state machine is destroyed.
         /// </summary>
-        /// <typeparam name="S">The Type of the derived Mechanic class</typeparam>
+        public void CleanUp()
+        {
+            foreach (KeyValuePair<Type, State<T>> entry in _stateLookUp)
+            {
+                entry.Value.CleanUp();
+            }
+        }
+
+        /// <summary>
+        /// Call every frame/tick.
+        /// </summary>
+        public void Update()
+        {
+            this._currentState.Update();
+        }
+
+        /// <summary>
+        /// Register a callback to be invoked every time the state machine switches states.
+        /// </summary>
+        public void RegisterSwitchStateCallback(Action<State<T>> callback)
+        {
+            this.StateSwitched += callback;
+        }
+
+        /// <summary>
+        /// Unregister a callback that was invoked every time the state machine switches states.
+        /// </summary>
+        public void UnregisterSwitchStateCallback(Action<State<T>> callback)
+        {
+            this.StateSwitched -= callback;
+        }
+
+
+        /// <summary>
+        /// Register a callback to be invoked every time the state machine switch fails.
+        /// </summary>
+        public void RegisterInvalidStateSwitchAttemptCallback(Action<Type> callback)
+        {
+            this.InvalidStateSwitchAttempt += callback;
+        }
+
+        /// <summary>
+        /// Unregister a callback that was invoked every time the state machine switch false.
+        /// </summary>
+        public void UnregisterInvalidStateSwitchAttemptCallback(Action<Type> callback)
+        {
+            this.InvalidStateSwitchAttempt -= callback;
+        }
+
+        /// <summary>
+        /// Automatically populate our states from the inner classes of type S.
+        /// </summary>
         public void SetStatesFromInnerClasses<S>()
         {
             Type[] myTypeArray;
@@ -64,78 +115,96 @@ namespace StateMechanics
             }
         }
 
+        /// <summary>
+        ///  Retrieve array of states in our machine of type S, where S inherits from State<T>.
+        /// </summary>
         public State<T>[] GetStates<S>() where S : State<T>
         {
-            return stateLookUp.Where(kvp => typeof(S).IsAssignableFrom(kvp.Key)).Select(kvp => kvp.Value).ToArray();
+            return this._stateLookUp.Where(kvp => typeof(S).IsAssignableFrom(kvp.Key)).Select(kvp => kvp.Value).ToArray();
         }
 
+        /// <summary>
+        /// Retrieve state of type S, where S inherits from State.
+        /// </summary>
         public S GetState<S>() where S : State<T>
         {
-            return stateLookUp.Where(kvp => typeof(S).IsAssignableFrom(kvp.Key)).Select(kvp => kvp.Value).FirstOrDefault() as S;
+            return this._stateLookUp.Where(kvp => typeof(S).IsAssignableFrom(kvp.Key)).Select(kvp => kvp.Value).FirstOrDefault() as S;
         }
 
+        /// <summary>
+        /// Add a state to our state machine of type S, where S inherits from State.
+        /// </summary>
         public void AddState<S>() where S : State<T>
         {
             Type stateType = typeof(S);
             State<T> newState = (State<T>)Activator.CreateInstance(stateType);
-            stateLookUp.Add(stateType, newState);
-            newState.SetMechanic(stateMechanic);
-            newState.SetStateMachine(this);
+            this._stateLookUp.Add(stateType, newState);
+            newState.InitFields(this, this._stateMechanic);
             newState.Init();
         }
 
-        public Type GetCurrentStateType()
-        {
-            return currentState.GetType();
-        }
-
-        public bool IsCurrentState<I>()
+        /// <summary>
+        /// Returns true if the current state is type S, where S inherits from State.
+        /// </summary>
+        public bool IsCurrentState<S>() where S : State<T>
         {
             bool match = false;
-            Type t = currentState.GetType();
+            Type t = this._currentState.GetType();
 
-            if (typeof(I) == t)
+            if (typeof(S) == t)
                 match = true;
 
             return match;
         }
 
-        public void SwitchTo<S>()
+        /// <summary>
+        /// Switch to the state of type S, where S inherits from State. If the state cannot be found, InvalidStateSwitchAttempt is invoked.
+        /// </summary>
+        public void SwitchTo<S>() where S : State<T>
         {
             Type StateType = typeof(S);
 
-            if (stateLookUp.TryGetValue(StateType, out State<T> nextState))
+            if (this._stateLookUp.TryGetValue(StateType, out State<T> nextState))
             {
                 ProcessNextState(nextState);
             }
+            else
+            {
+                this.InvalidStateSwitchAttempt?.Invoke(typeof(S));
+            }
         }
 
-
+        /// <summary>
+        /// Switch to state of type State<T>. If the state cannot be found, InvalidStateSwitchAttempt is invoked.
+        /// </summary>
         public void SwitchTo(State<T> nextState)
         {
-            if (stateLookUp.ContainsValue(nextState))
+            if (this._stateLookUp.ContainsValue(nextState))
             {
                 ProcessNextState(nextState);
+            }
+            else
+            {
+                this.InvalidStateSwitchAttempt?.Invoke(nextState.GetType());
             }
         }
 
         private void AddStateType(Type stateType)
         {
             State<T> newState = (State<T>)Activator.CreateInstance(stateType);
-            stateLookUp.Add(stateType, newState);
-            newState.SetMechanic(stateMechanic);
-            newState.SetStateMachine(this);
+            this._stateLookUp.Add(stateType, newState);
+            newState.InitFields(this, this._stateMechanic);
             newState.Init();
         }
 
 
         private void ProcessNextState(State<T> nextState)
         {
-            stateSwitch?.Invoke(nextState);
+            this.StateSwitched?.Invoke(nextState);
 
-            currentState?.Exit();
-            currentState = nextState;
-            currentState.Enter();
+            this._currentState?.Exit();
+            this._currentState = nextState;
+            this._currentState.Enter();
         }
     }
 
